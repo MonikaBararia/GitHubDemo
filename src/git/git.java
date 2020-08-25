aee8647e49d9821661004350da481a12a911874c
bmonaswit <bmonaswit@gmail.com>
2020-08-24 23:22:53 -0700

    New commit

--- src/git/git.java
+++ src/git/git.java
@@ -5,6 +5,8 @@ public class git {
 	public static void main(String[] args) {
 		System.out.println("My name is Git");
 		System.out.println("My name is Sam");
+		System.out.println("My name is Riya");
+		
 
 	}
 
